4bf596141cdd6bfecd455daba8ef1c722f9493d0
Masmat97 <matteo.mascio@libero.it>
2024-10-17 10:27:33 +0200

    end

--- main.js
+++ main.js
@@ -1,36 +1,78 @@
-const age = prompt("Inserisci la tua età");
-console.log(`Anni: ${age}`);
-
-const km = prompt("Inserisci il numero di km da percorrere")
-console.log(`km: ${km}`);
-
-const prezzo = 0.21;
-console.log(`prezzo: ${prezzo}`);
+const kmInput = document.getElementById('km');
+const ageInput = document.getElementById('age');
 
 
 
-const prezzoBiglietto = prezzo * km;
-console.log(`prezzoBiglietto: ${prezzoBiglietto}`);
-document.getElementById("biglietto").innerHTML = `prezzoBiglietto: ${prezzoBiglietto}`
+// const calcButton = document.getElementById('calc-button');
+
+// calcButton.addEventListener('click', function () {
+
+//     const km = kmInput.value;
+//     const age = ageInput.value;
+
+//     const costPerKm = 0.21;
+//     const basePrice = km * costPerKm;
+//     console.log(`Prezzo base biglietto`, basePrice);
+
+//     let finalPrice = basePrice;
+
+//     if (age < 18) {
+//         console.log('Applico lo sconto del 20%');
+
+//         finalPrice = basePrice * 0.8;
+//         console.log('Prezzo scontato del 20%:', finalPrice);
+//     }
+//     else if (age > 65) {
+//         console.log('Applico lo sconto del 40%');
+
+//         finalPrice = basePrice * 0.6;
+//         console.log('Prezzo scontato del 40%:', finalPrice);
+//     }
+
+//     console.log('Prezzo finale', finalPrice.toFixed(2));
+// });
 
 
 
-if (age < 18)  {
-    console.log(`Applico lo sconto del 20%`);
-   
-    const finalPrice = prezzoBiglietto * 0.8;
-    console.log(`Prezzo sconato del 20%:`, finalPrice);
-    document.getElementById("biglietto").innerHTML = `Prezzo sconato del 20%:`, finalPrice
-
-}
 
 
-else if (age > 65)  {
-    console.log(`Applico lo sconto del 40%`);
-   
+const priceForm = document.querySelector('form');
 
-    const finalPrice = prezzoBiglietto * 0.6;
-    console.log(`Prezzo sconato del 40%:`, finalPrice);
-    document.getElementById("biglietto").innerHTML = `Prezzo sconato del 40%:`, finalPrice
+priceForm.addEventListener('submit', function (prova) {
+    prova.preventDefault();
+    
 
-}
+    const calcButton = document.getElementById('calc-button');
+
+    calcButton.addEventListener('click', function () {
+
+        const km = kmInput.value;
+        const age = ageInput.value;
+
+        const costPerKm = 0.21;
+        const basePrice = km * costPerKm;
+        console.log(`Prezzo base biglietto`, basePrice);
+
+        let finalPrice = basePrice;
+
+        if (age < 18) {
+            console.log('Applico lo sconto del 20%');
+
+            finalPrice = basePrice * 0.8;
+            console.log('Prezzo scontato del 20%:', finalPrice);
+        }
+        else if (age > 65) {
+            console.log('Applico lo sconto del 40%');
+
+            finalPrice = basePrice * 0.6;
+            console.log('Prezzo scontato del 40%:', finalPrice);
+        }
+        else {
+            alert('Dati non validi')
+        }
+        document.getElementById("biglietto").innerHTML = 'Prezzo del biglietto:' + finalPrice.toFixed(2) + '€';
+        // document.getElementById('km').value = '';
+        // document.getElementById('age').value = '';
+        
+    });
+});
